4f38df6eecb4dab6d8b7764551e376bdc4fcd445
MaximeGrolleau <grolleau.maxime@gmail.com>
2015-01-05 07:03:00 +0100

    Ajout du rapport

--- src/images/detection.m
+++ src/images/detection.m
@@ -1,10 +1,17 @@
 function detection()
 
- hedge = vision.EdgeDetector;
+    hcontadj = vision.ContrastAdjuster;
+       x = imread('5.jpg');
+       x = rgb2gray(x);
+       y = step(hcontadj, x);
+       subplot(1,2,1); imshow(x); title('Original Image');
+       subplot(1,2,2); imshow(y); title('Contrast-adjusted Image');
+
+       hedge = vision.EdgeDetector;
        hcsc = vision.ColorSpaceConverter(...
         'Conversion', 'RGB to intensity');
        hidtypeconv = vision.ImageDataTypeConverter('OutputDataType', 'single');
-       img = step(hcsc, imread('0.jpg'));
+       img = step(hcsc,x);
        img1 = step(hidtypeconv, img);
        edges = step(hedge, img1);
        imshow(edges);
